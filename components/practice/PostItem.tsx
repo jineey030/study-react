7b7930f159e2f40310f2d09c5e57b777a09d3a82
jineey030 <92289949+jineey030@users.noreply.github.com>
2026-08-31 16:43:49 +0900

    feat: 삭제 기능 추가

--- components/practice/PostItem.tsx
+++ components/practice/PostItem.tsx
@@ -10,9 +10,10 @@ interface Post {
 interface PostItemProps {
   post: Post;
   onDetail: (id: number) => void;
+  onDelete: (id: number) => void;
 }
 
-export default function PostItem ( {post, onDetail} :PostItemProps) {
+export default function PostItem ( {post, onDetail, onDelete} :PostItemProps) {
     return (
         <li className="mb-4">
             <div
@@ -21,6 +22,11 @@ export default function PostItem ( {post, onDetail} :PostItemProps) {
             >
                 {post.id}. {post.title}
             </div>
+            <button 
+                    className="px-2.5 py-1 text-xs font-medium text-white bg-red-500 rounded-md hover:bg-red-600 active:scale-95 transition whitespace-nowrap"
+                    onClick={() => onDelete(post.id)}
+                >
+                삭제</button>
         </li>
     );
 }
