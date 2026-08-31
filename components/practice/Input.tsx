b1e59ff6e4c79d53300ce768384824a49126b437
jineey030 <92289949+jineey030@users.noreply.github.com>
2026-08-31 13:15:10 +0900

    Refactor: 디자인 변경

--- components/practice/Input.tsx
+++ components/practice/Input.tsx
@@ -4,9 +4,7 @@ interface InputProps {
   onInputText(txt: string): void;
 }
 
-export default function Input(
-  {onInputText}: InputProps
-) {
+export default function Input({ onInputText }: InputProps) {
   const [text, setText] = useState('');
 
   const handleSubmit = () => {
@@ -15,17 +13,23 @@ export default function Input(
   };
 
   return (
-    <div>
-       <input 
-        value={text} 
-        onChange={(e) => setText(e.target.value)} 
-        placeholder="값을 입력하세요"
-        className="px-3 py-1 border rounded-md dark:bg-zinc-850 dark:border-zinc-700"
-      />
-       <button 
-        onClick={handleSubmit}
-        className="px-4 py-2 font-semibold text-white bg-blue-500 rounded-lg hover:bg-blue-600 active:scale-95 transition"
-       >제출</button>
+    <div className="flex flex-col gap-2">
+      <h2 className="text-sm font-semibold text-zinc-600 dark:text-zinc-400">텍스트 입력</h2>
+      
+      <div className="flex gap-2">
+        <input 
+          value={text} 
+          onChange={(e) => setText(e.target.value)} 
+          placeholder="값을 입력하세요"
+          className="px-3 py-2 text-zinc-700 dark:text-zinc-200 bg-white dark:bg-zinc-800 border border-zinc-200 dark:border-zinc-700 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 transition shadow-sm"
+        />
+        <button 
+          onClick={handleSubmit}
+          className="px-4 py-2 font-medium text-white bg-blue-500 hover:bg-blue-600 rounded-lg active:scale-95 transition shadow-sm"
+        >
+          제출
+        </button>
+      </div>
     </div>
   );
 }
